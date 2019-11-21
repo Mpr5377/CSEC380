cfecbc42929ed7cb843af47bb61a714282d63ea8
Mpr5377 <35986674+Mpr5377@users.noreply.github.com>
2019-11-20 22:46:32 -0500

    Fixed Block comment warning

--- test_project.py
+++ test_project.py
@@ -6,53 +6,53 @@ def test_act3_authentication():
     x = 5
     y = 5
 
-    assert x != y, "test failed"
-    #TODO
+    assert x == y
+    # TODO
     # Test 1: Logs into the application successfully
 
-    #TODO
+    # TODO
     # Test 2: Tries to log into the application with wrong password and fails
 
-    #TODO
+    # TODO
     # Test 3: Tries to log into the application with wrong username and fails
 
 
 def test_act4_content():
     pass
 
-    #TODO
+    # TODO
     # Logs into the account successfully
 
-    #TODO
+    # TODO
     # Uploads a new video
 
-    #TODO
+    # TODO
     # Access/Make sure video exists
 
-    #TODO
+    # TODO
     # Deletes the video
 
 
 def test_act5_sql_injection():
     pass
 
-    #TODO
+    # TODO
     # Vulnerable to classic SQL
 
-    #TODO
+    # TODO
     # Vulnerable to blind SQL
 
 
 def test_act6_ssrf():
     pass
-    #TODO
+    # TODO
     # Vulnerable to SSRF
 
 
 def test_act7_command_injection():
     pass
 
-    #TODO
+    # TODO
     # Vulnerable to Command Injection
 
 
